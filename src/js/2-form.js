9b629cbc0bc1ce68c2593b55c14c2c27d74511cf
ArtemSv07 <135955009+ArtemSv07@users.noreply.github.com>
2024-01-18 21:42:23 +0200

    hw-9, task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -14,7 +14,7 @@ try{
    })
   
 } catch(error){
-  
+  console.log("parse error");
 }
 
 form.addEventListener("input", () => {
@@ -26,16 +26,31 @@ form.addEventListener("input", () => {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formObject))
 });
 
+
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-  localStorage.removeItem(STORAGE_KEY);
-  form.reset();
+  event.preventDefault();
+
+
+  const emailInput = form.elements["email"];
+  const messageInput = form.elements["message"];
+
+  if (emailInput.checkValidity() && messageInput.checkValidity()) {
+
+    const formData = new FormData(form);
+    const formObject = {};
+    
+    formData.forEach((value, key) => {
+      formObject[key] = value.trim();
+    });
+
+    
+    console.log("Form Object:", formObject);
+
+    
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+  } else {
+    
+    console.log("Invalid form data. Please check your inputs.");
+  }
 });
- 
-
-
-
-
-
-
-
