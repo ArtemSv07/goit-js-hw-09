4b10a70e724e00c8aa26d6cd198ac0c2a49d50fc
ArtemSv07 <135955009+ArtemSv07@users.noreply.github.com>
2024-01-20 09:28:48 +0200

    hw-9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,56 +1,66 @@
-const STORAGE_KEY = "feedback-form-state";
 
+const STORAGE_KEY = "feedback-form-state";
 const form = document.querySelector(".feedback-form");
 
 
-try{
-  const initialForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
- 
-   Array.from(form.elements).forEach(element =>{
-    const storValue = initialForm[element.name];
-    if(storValue){
-      element.value = storValue;
-    }
-   })
+
+  // "Function to check if all form fields are filled"
+  function isFormFilled() {
+    const formData = new FormData(form);
+    return Array.from(formData.values()).every((value) => value.trim() !== "");
+  }
   
-} catch(error){
+
+try {
+  const initialForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  Array.from(form.elements).forEach((element) => {
+    const storedValue = initialForm[element.name];
+    if (storedValue) {
+      element.value = storedValue;
+    }
+  });
+} catch (error) {
   console.log("parse error");
 }
 
 form.addEventListener("input", () => {
-  const formData = new FormData(form);
-  const formObject = {};
-  formData.forEach((value, key) => {
-    formObject[key] = value.trim(); 
-  })
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formObject))
-});
-
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-
-  const emailInput = form.elements["email"];
-  const messageInput = form.elements["message"];
-
-  if (emailInput.checkValidity() && messageInput.checkValidity()) {
-
+  if (isFormFilled()) {
     const formData = new FormData(form);
     const formObject = {};
-    
+
     formData.forEach((value, key) => {
       formObject[key] = value.trim();
     });
 
-    
-    console.log("Form Object:", formObject);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formObject));
+  } else {
+    localStorage.removeItem(STORAGE_KEY);
+  }
+});
 
-    
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const emailInput = form.elements["email"];
+  const messageInput = form.elements["message"];
+
+
+  if (emailInput.checkValidity() && messageInput.checkValidity() && isFormFilled()) {
+    const formData = new FormData(form);
+    const formObject = {};
+
+    formData.forEach((value, key) => {
+      formObject[key] = value.trim();
+    });
+
+    console.log("Object form:", formObject);
+
     localStorage.removeItem(STORAGE_KEY);
     form.reset();
   } else {
-    
-    console.log("Invalid form data. Please check your inputs.");
+    alert ("Enter your email and message, both input fields are mandatory")
   }
 });
+
+
